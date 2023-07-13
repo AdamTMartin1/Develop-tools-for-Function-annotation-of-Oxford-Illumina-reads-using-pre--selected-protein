22cc607feca567efed6ea2407957ca742bc40099
AdamTMartin1 <2414054@dundee.ac.uk>
2023-07-13 13:42:21 +0100

    Delete Program Steps directory

--- Program Steps/Step4MakesReferenceDatabaseForBlasting.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$ -cwd
-#S -v
-#$ -b n
-
-cd
-cd /home/ws152103/Documents/Project/ReferenceData
-makeblastdb -in AllSequences.fa -dbtype prot -out mygenome -title  'Description of the genome'
